bb93e16ee12964bd0ee81bbddb95cb0b38c49247
RiteshSarathe-TSM <ritesh.sarathe@stackmentalist.com>
2025-07-21 18:13:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ function App() {
   return (
     <div
       style={{
-        height: '100vh',
+        minHeight: '100vh',
+        margin: 0,
+        padding: 0,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#1e1e1e',
+        textAlign: 'center',
       }}
     >
       <h1 style={{ color: 'white', fontSize: '2.5rem', fontWeight: 'bold' }}>
