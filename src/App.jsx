4dd8fee1fc99e21dce210be09188294674e67c7f
RiteshSarathe-TSM <ritesh.sarathe@stackmentalist.com>
2025-07-21 18:18:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ function App() {
   return (
     <div
       style={{
-        minHeight: '100vh',
+        height: '100vh', // full height of viewport
         margin: 0,
         padding: 0,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#1e1e1e',
-        textAlign: 'center',
+        backgroundColor: '#1e1e1e', // dark background
       }}
     >
-      <h1 style={{ color: 'white', fontSize: '2.5rem', fontWeight: 'bold' }}>
+      <h1 style={{ 
+        color: 'white', 
+        fontSize: '2.5rem', 
+        fontWeight: 'bold',
+        textAlign: 'center' 
+      }}>
         Hello Matson, I am Vishal
       </h1>
     </div>
