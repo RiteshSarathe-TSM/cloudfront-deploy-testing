a640f0d2887febc843fc024e4f201d924ad38855
RiteshSarathe-TSM <ritesh.sarathe@stackmentalist.com>
2025-07-21 18:08:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 function App() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '40vh', justify: 'center', display: 'flex' }}>
-      <h1>Hello Matson, I am Vishal</h1>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#1e1e1e',
+        color: 'white',
+        fontSize: '2rem',
+        fontWeight: 'bold',
+      }}
+    >
+      Hello Matson, I am Vishal
     </div>
   );
 }
